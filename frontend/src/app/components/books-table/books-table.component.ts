e50e174ab81437ac2f73be57acece0785377ed9e
Art <artjohanaaspollu@gmail.com>
2023-05-18 11:44:45 +0300

    added pagination and sorting using the backend API requestparams

--- frontend/src/app/components/books-table/books-table.component.ts
+++ frontend/src/app/components/books-table/books-table.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { Observable } from 'rxjs';
-import { Page } from '../../models/page';
+import { Page, PageRequest } from '../../models/page';
 import { Book } from '../../models/book';
-import { MatTableDataSource } from '@angular/material/table'
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort, Sort} from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-books-table',
@@ -12,19 +14,45 @@ import { MatTableDataSource } from '@angular/material/table'
 })
 export class BooksTableComponent implements OnInit {
 
-  constructor(private bookService: BookService) {
+  constructor(private bookService: BookService) { }
 
+  pageInfo: PageRequest = {
+    pageIndex: 0,
+    pageSize: 20,
+    sort: '',
+    direction: ''
   }
 
-  books$!: Observable<Page<Book>>;
+  allBooks$!: Observable<Page<Book>>
+  booksLength?: number
   dataSource = new MatTableDataSource<Book>()
   columnHeaders: string[] = ['title', 'author', 'year', 'genre']
 
   // making the dataSource an array of the book objects to populate the mat table
   ngOnInit(): void {
-    this.books$ = this.bookService.getBooks({})
-    this.books$.subscribe(books => {
-      this.dataSource = new MatTableDataSource<Book>(books.content)
+    this.updateTable()
+    this.allBooks$ = this.bookService.getBooks({pageIndex: 0, pageSize: this.booksLength})
+  }
+
+  // pagination using the api, changing page index and size to the paginator values
+  pagination(event:PageEvent): void {
+    this.pageInfo.pageIndex = event.pageIndex
+    this.pageInfo.pageSize = event.pageSize
+    this.updateTable()
+  }
+
+  // sorting using the api, changing page sort and direction to the sorter values
+  sortTable(event:Sort): void {
+    this.pageInfo.sort = event.active
+    this.pageInfo.direction = event.direction
+    this.updateTable()
+  }
+
+  // updates the table by updating the dataSource according to the pageInfo criteria
+  updateTable(): void {
+    this.bookService.getBooks(this.pageInfo).subscribe(books => {
+      this.dataSource.data = books.content
+      this.booksLength = books.totalElements
     })
   }
 }
